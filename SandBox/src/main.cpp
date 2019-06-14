bcf1aeb46c1a7e624b55c48a41aa46f21ee2b5ec
hossein azizollahi <h.azizollahi@gmail.com>
2019-06-14 20:49:57 +0430

    initiate

---
+++ SandBox/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "../../include/GES/Test.h"
+
+
+int main() {
+	Test test;
+	test.print();
+	return 0;
+}
---
+++ include/GES/Test.h
@@ -0,0 +1,17 @@
+//
+// Created by developer on 6/14/19.
+//
+
+#ifndef SIMPLE_TEST_H
+#define SIMPLE_TEST_H
+
+
+class Test {
+
+public:
+	void print();
+
+};
+
+
+#endif //SIMPLE_TEST_H
